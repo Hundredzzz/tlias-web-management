faba30f82620b568e30e1cbe15d253086b32f94f
hundredz <1472212805@qq.com>
2026-04-09 13:41:03 +0800

    Initial commit

--- src/main/java/com/hundredz/mapper/OperateLogMapper.java
+++ src/main/java/com/hundredz/mapper/OperateLogMapper.java
@@ -1,6 +1,5 @@
 package com.hundredz.mapper;
 
-<<<<<<< HEAD
 import com.hundredz.pojo.LogQueryParam;
 import com.hundredz.pojo.OperateLog;
 import org.apache.ibatis.annotations.Insert;
@@ -9,27 +8,20 @@ import org.apache.ibatis.annotations.Select;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
-=======
 import com.hundredz.pojo.OperateLog;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
->>>>>>> befb783f2ea6acee673c5b65508aad7c715ea16b
+
 
 @Mapper
 public interface OperateLogMapper {
 
-<<<<<<< HEAD
 
-=======
->>>>>>> befb783f2ea6acee673c5b65508aad7c715ea16b
     //插入日志数据
     @Insert("insert into operate_log (operate_emp_id, operate_time, class_name, method_name, method_params, return_value, cost_time) " +
             "values (#{operateEmpId}, #{operateTime}, #{className}, #{methodName}, #{methodParams}, #{returnValue}, #{costTime});")
     public void insert(OperateLog log);
 
-<<<<<<< HEAD
     @Select("select * from operate_log ")
     List<OperateLog> list(LogQueryParam logQueryParam);
-=======
->>>>>>> befb783f2ea6acee673c5b65508aad7c715ea16b
 }
