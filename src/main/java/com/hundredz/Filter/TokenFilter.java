091b8f72f0417a1ef938f147236b4c09b7682f9a
hundredz <1472212805@qq.com>
2026-04-13 11:38:22 +0800

    Initial commit

--- src/main/java/com/hundredz/Filter/TokenFilter.java
+++ src/main/java/com/hundredz/Filter/TokenFilter.java
@@ -1,12 +1,9 @@
 package com.hundredz.Filter;
 
-<<<<<<< HEAD
 import com.hundredz.utils.CurrentHolder;
 import com.hundredz.utils.JwtUtils;
 import io.jsonwebtoken.Claims;
-=======
-import com.hundredz.utils.JwtUtils;
->>>>>>> befb783f2ea6acee673c5b65508aad7c715ea16b
+
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
@@ -47,16 +44,16 @@ public class TokenFilter implements Filter {
 
         // 5. 如果存在，校验令牌，如果校验令牌失败， 返回错误信息
         try {
-<<<<<<< HEAD
+
             Claims claims = JwtUtils.parseJWT(token);
             Integer empId = Integer.valueOf(claims.get("id").toString());
             CurrentHolder.setCurrentId(empId);
             log.info("当前登录员工ID: {}, 将其存入ThreadLoacl", empId);
 
 
-=======
+
             JwtUtils.parseJWT(token);
->>>>>>> befb783f2ea6acee673c5b65508aad7c715ea16b
+
         } catch (Exception e) {
             log.info("令牌为空，响应401状态码");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
@@ -67,11 +64,9 @@ public class TokenFilter implements Filter {
         // 6. 如果校验令牌成功，放行
         log.info("令牌校验成功，放行");
         filterChain.doFilter(request, response);
-<<<<<<< HEAD
 
         // 7. 请求结束后，移除ThreadLocal中的数据
         CurrentHolder.remove();
-=======
->>>>>>> befb783f2ea6acee673c5b65508aad7c715ea16b
+
     }
 }
